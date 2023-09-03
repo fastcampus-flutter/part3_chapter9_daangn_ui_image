073f7e0e38720887ac12b1d73984b1bd0b827b06
Bansook Nam <bs.nam@lawfully.com>
2023-09-03 22:30:10 +0900

    fab 분기 처리 추가

--- lib/screen/main/fab/w_floating_daangn_button.dart
+++ lib/screen/main/fab/w_floating_daangn_button.dart
@@ -1,8 +1,11 @@
 import 'package:fast_app_base/common/cli_common.dart';
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/screen/main/fab/floating_small_provider.dart';
+import 'package:fast_app_base/screen/main/s_main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../tab/tab_item.dart';
 
 final floatingExpandedProvider = StateProvider((ref) {
   return false;
@@ -32,57 +35,63 @@ class _FloatingDaangnButtonState extends ConsumerState<FloatingDaangnButton> {
         )),
         Align(
           alignment: Alignment.bottomRight,
-          child: Tap(
-            onTap: () {
-              ref.read(floatingExpandedProvider.notifier).state = !isExpanded;
-            },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                AnimatedOpacity(
-                  opacity: isExpanded ? 1 : 0,
-                  duration: 100.ms,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Container(
-                        width: 160,
-                        padding: const EdgeInsets.all(15),
-                        margin: const EdgeInsets.only(right: 15, bottom: 10),
-                        decoration: BoxDecoration(
-                            color: context.appColors.floatingActionLayer,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            _floatItem('알바', '$basePath/fab/fab_01.png'),
-                            _floatItem('과외/클래스', '$basePath/fab/fab_02.png'),
-                            _floatItem('농수산물', '$basePath/fab/fab_03.png'),
-                            _floatItem('부동산', '$basePath/fab/fab_04.png'),
-                            _floatItem('중고차', '$basePath/fab/fab_05.png'),
-                          ],
-                        ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AnimatedOpacity(
+                opacity: isExpanded ? 1 : 0,
+                duration: 100.ms,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: 160,
+                      padding: const EdgeInsets.all(15),
+                      margin: const EdgeInsets.only(right: 15, bottom: 10),
+                      decoration: BoxDecoration(
+                          color: context.appColors.floatingActionLayer,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          _floatItem('알바', '$basePath/fab/fab_01.png'),
+                          _floatItem('과외/클래스', '$basePath/fab/fab_02.png'),
+                          _floatItem('농수산물', '$basePath/fab/fab_03.png'),
+                          _floatItem('부동산', '$basePath/fab/fab_04.png'),
+                          _floatItem('중고차', '$basePath/fab/fab_05.png'),
+                        ],
                       ),
-                      const Height(3),
-                      Container(
-                        width: 160,
-                        padding: const EdgeInsets.all(15),
-                        margin: const EdgeInsets.only(right: 15, bottom: 10),
-                        decoration: BoxDecoration(
-                            color: context.appColors.floatingActionLayer,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            _floatItem('내 물건 팔기', '$basePath/fab/fab_06.png'),
-                          ],
-                        ),
+                    ),
+                    const Height(3),
+                    Container(
+                      width: 160,
+                      padding: const EdgeInsets.all(15),
+                      margin: const EdgeInsets.only(right: 15, bottom: 10),
+                      decoration: BoxDecoration(
+                          color: context.appColors.floatingActionLayer,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          _floatItem('내 물건 팔기', '$basePath/fab/fab_06.png'),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                AnimatedContainer(
+              ),
+              Tap(
+                onTap: () {
+                  final currentTab = ref.read(currentTabProvider);
+                  if(currentTab == TabItem.localLife){
+                    context.showSnackbar('wow');
+                    return;
+                  }
+
+                  ref.read(floatingExpandedProvider.notifier).state = !isExpanded;
+                },
+                child: AnimatedContainer(
                   height: 60,
                   duration: 100.ms,
                   padding: const EdgeInsets.symmetric(horizontal: 18),
@@ -101,9 +110,9 @@ class _FloatingDaangnButtonState extends ConsumerState<FloatingDaangnButton> {
                       )
                     ],
                   ),
-                ).pOnly(bottom: 80, right: 15),
-              ],
-            ),
+                ),
+              ).pOnly(bottom: 80, right: 15),
+            ],
           ),
         )
       ],
--- lib/screen/main/s_main.dart
+++ lib/screen/main/s_main.dart
@@ -1,3 +1,4 @@
+import 'package:fast_app_base/common/cli_common.dart';
 import 'package:fast_app_base/screen/main/fab/w_floating_daangn_button.dart';
 import 'package:fast_app_base/screen/main/tab/tab_item.dart';
 import 'package:fast_app_base/screen/main/tab/tab_navigator.dart';
@@ -7,18 +8,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../common/common.dart';
 import 'w_menu_drawer.dart';
 
-class MainScreen extends StatefulWidget {
+final currentTabProvider = StateProvider((ref) => TabItem.home);
+
+class MainScreen extends ConsumerStatefulWidget {
   const MainScreen({super.key});
 
   @override
-  State<MainScreen> createState() => MainScreenState();
+  ConsumerState<MainScreen> createState() => MainScreenState();
 }
 
-class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin {
-  TabItem _currentTab = TabItem.home;
+class MainScreenState extends ConsumerState<MainScreen> with SingleTickerProviderStateMixin {
   final tabs = TabItem.values;
   late final List<GlobalKey<NavigatorState>> navigatorKeys =
       TabItem.values.map((e) => GlobalKey<NavigatorState>()).toList();
+
+  TabItem get _currentTab => ref.watch(currentTabProvider);
 
   int get _currentIndex => tabs.indexOf(_currentTab);
 
@@ -37,29 +41,31 @@ class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMi
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      child: WillPopScope(
-        onWillPop: _handleBackPressed,
-        child: Material(
-          child: Stack(
-            children: [
-              Scaffold(
-                extendBody: extendBody, //bottomNavigationBar 아래 영역 까지 그림
-                drawer: const MenuDrawer(),
-                body: Container(
-                  color: context.appColors.seedColor.getMaterialColorValues[200],
-                  padding: EdgeInsets.only(
-                      bottom: extendBody ? 60 - bottomNavigationBarBorderRadius : 0),
-                  child: SafeArea(
-                    bottom: !extendBody,
-                    child: pages,
-                  ),
+    return WillPopScope(
+      onWillPop: _handleBackPressed,
+      child: Material(
+        child: Stack(
+          children: [
+            Scaffold(
+              extendBody: extendBody, //bottomNavigationBar 아래 영역 까지 그림
+              drawer: const MenuDrawer(),
+              body: Container(
+                color: context.appColors.seedColor.getMaterialColorValues[200],
+                padding:
+                    EdgeInsets.only(bottom: extendBody ? 60 - bottomNavigationBarBorderRadius : 0),
+                child: SafeArea(
+                  bottom: !extendBody,
+                  child: pages,
                 ),
-                bottomNavigationBar: _buildBottomNavigationBar(context),
               ),
-              FloatingDaangnButton(),
-            ],
-          ),
+              bottomNavigationBar: _buildBottomNavigationBar(context),
+            ),
+            AnimatedOpacity(
+              duration: 200.ms,
+              opacity: _currentIndex == 3 ?0: 1,
+              child: FloatingDaangnButton(),
+            ),
+          ],
         ),
       ),
     );
@@ -128,9 +134,7 @@ class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMi
   }
 
   void _changeTab(int index) {
-    setState(() {
-      _currentTab = tabs[index];
-    });
+    ref.read(currentTabProvider.notifier).state = tabs[index];
   }
 
   BottomNavigationBarItem bottomItem(
